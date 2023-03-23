c5d36dec27b40da4937728928292f5a3686e13c5
Alexis Montilla <alexisamm9261@gmail.com>
2023-03-23 13:01:38 -0400

    feature: env variables added to database conection

--- database/conection.ts
+++ database/conection.ts
@@ -1,10 +1,13 @@
 import {Sequelize} from 'sequelize';
+import dotenv from "dotenv"
 
-const db = new Sequelize('social_network', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    logging: true,
+dotenv.config()
+
+const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    logging: process.env.DB_DEBUG === "true" ? true : false,
 });
 
 export default db;
--- dist/database/conection.js
+++ dist/database/conection.js
@@ -1,11 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('social_network', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    logging: true,
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const db = new sequelize_1.Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    logging: process.env.DB_DEBUG === "true" ? true : false,
 });
 exports.default = db;
 //# sourceMappingURL=conection.js.map
