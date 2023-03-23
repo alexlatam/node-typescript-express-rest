2b6f18057fd910f6ab72640abe54cf8aa1c77983
Alexis Montilla <alexisamm9261@gmail.com>
2023-03-22 22:46:42 -0400

    start project with typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.nombre = void 0;
+// index.ts
+exports.nombre = 'Test';
+console.log(exports.nombre);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,4 @@
+// index.ts
+export const nombre = 'Test'
+
+console.log(nombre)
